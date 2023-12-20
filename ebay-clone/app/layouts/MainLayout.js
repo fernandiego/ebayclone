438cf3fdd412bab7ff6c80fbb9b3fd62abe1f467
fox <fernandodiego.sn@gmail.com>
2023-12-20 16:25:19 -0300

    Setting up the layouts

--- ebay-clone/app/layouts/MainLayout.js
+++ ebay-clone/app/layouts/MainLayout.js
@@ -1,10 +1,11 @@
 "use client"
+import TopMenu from "@/app/layouts/includes/TopMenu";
 
 export default function MainLayout({children}) {
     return (
         <>
-            <div id="MainLayout">
-
+            <div id="MainLayout" className='min-w-[1050px] max-w-[1300px] mx-auto'>
+                <TopMenu />
             </div>
         </>
     )
