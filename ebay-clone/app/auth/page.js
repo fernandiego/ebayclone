9d9449001391223d1973dd6120e8448942975794
fox <fernandodiego.sn@gmail.com>
2023-12-29 15:39:03 -0300

    feat(new page): Auth page, and orders page

---
+++ ebay-clone/app/auth/page.js
@@ -0,0 +1,26 @@
+"use client"
+
+import React from "react";
+import {Auth} from "@supabase/auth-ui-react";
+import {ThemeSupa} from "@supabase/auth-ui-shared";
+import Link from 'next/link'
+
+export default function AuthPage() {
+
+
+    return (
+        <>
+            <div id="AuthPage" className="w-full min-h-screen bg-white">
+                <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
+                    <Link href="/" className="min-w-[170px]">
+                        <img width="170" src="/images/logo.svg"/>
+                    </Link>
+                </div>
+                <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
+                    Login / Register
+                </div>
+                <div className="max-w-[400px] mx-auto px2"></div>
+            </div>
+        </>
+    )
+}
